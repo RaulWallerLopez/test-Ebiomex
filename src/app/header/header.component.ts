e11c9aea8f2bcba0733b47318a370c49b177eaad
RaulWallerLopez <raulvaler@gmail.com>
2021-02-13 18:55:24 -0600

    Start

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+    $(document).ready(function(){
+      //animaciones de header
+      $(window).scroll(function() {
+        if ($("#menu").offset().top > 56) {
+            $("#menu").addClass("bg-white");
+            $('.logo').css('filter','invert(0%)');
+            $('.myNav a').css('color','black');
+        } else {
+            $("#menu").removeClass("bg-white");
+            $('.logo').css('filter','invert(100%)');
+            $('.myNav a').css('color','white');
+        }
+      });
+      //disparo de animaciones
+      $(window).scroll(function() {
+        if ($("#menu").offset().top > 1150) {
+            $(".cardEqu").addClass("animate__animated animate__zoomIn");
+        }
+        else
+        {
+          $(".cardEqu").removeClass("animate__animated animate__zoomIn");
+        }
+      });
+      $(window).scroll(function() {
+        if ($("#menu").offset().top > 2450) {
+            $(".cardIns").addClass("animate__animated animate__zoomIn");
+        }
+        else
+        {
+          $(".cardIns").removeClass("animate__animated animate__zoomIn");
+        }
+      });
+      //cambiar ek focus de la nav bar
+      $(".navbar-nav .nav-link").on("click", function(){
+        $(".navbar-nav").find(".active").removeClass("active");
+        $(this).addClass("active");
+     });
+     $("#Inicio").hover(function(){
+      $(".navbar-nav").find(".active").removeClass("active");
+      $(".Inicio").addClass("active");
+     });
+     $("#Nosotros").hover(function(){
+      $(".navbar-nav").find(".active").removeClass("active");
+      $(".Nosotros").addClass("active");
+     });
+     $("#Equipos").hover(function(){
+      $(".navbar-nav").find(".active").removeClass("active");
+      $(".Equipos").addClass("active");
+     });
+     $("#Insumos").hover(function(){
+      $(".navbar-nav").find(".active").removeClass("active");
+      $(".Insumos").addClass("active");
+     });
+     $("#Mantenimiento").hover(function(){
+      $(".navbar-nav").find(".active").removeClass("active");
+      $(".Mantenimiento").addClass("active");
+     });
+     $("#Contacto").hover(function(){
+      $(".navbar-nav").find(".active").removeClass("active");
+      $(".Contacto").addClass("active");
+     });
+    })
+  }
+
+}
